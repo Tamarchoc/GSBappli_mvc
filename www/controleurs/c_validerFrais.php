1880407d4a7ff3ae9de07c20610f5eb64b0047c8
tamar chocron <tamarchoc@gmail.com>
2023-12-25 17:39:47 +0100

    COMMIT 1

--- www/controleurs/c_validerFrais.php
+++ www/controleurs/c_validerFrais.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Gestion de la connexion
  *
@@ -13,32 +14,98 @@
  * @version   GIT: <0>
  * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
  */
-
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
-
+$mois = getMois(date('d/m/Y'));
 
 switch ($action) {
-    
-case 'validerFrais':
+
+    case 'selectionnerVisiteursMois':
+
         $visiteurs_a_valider = $pdo->getLesVisiteursAValider();
+        $lesCles = array_keys($visiteurs_a_valider);
+        $moisASelectionner = $lesCles[0];
+        $mois_a_selectionner = getLesDouzeDerniersMois($mois);
+        $lesCles = array_keys($mois_a_selectionner);
+        $moisASelectionner = $lesCles[0];
+        include 'vues/v_listeMoisVisiteur.php';
+
+        break;
+
+    case 'afficherDonnees' :
+        $idVisiteur = filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING);
+        $unMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+        //var_dump($unMois, $idVisiteur);
+        $visiteurs_a_valider = $pdo->getLesVisiteursAValider();
+        $visiteurASelectionner = $idVisiteur;
         // var_dump($visiteurs_a_valider);
-        $mois_a_selectionner =getMoisASelectionner();
+        $mois_a_selectionner =getLesDouzeDerniersMois($mois);
+        $moisASelectionner = $unMois;
         // var_dump($mois_a_selectionner);
-        include 'vues/v_validerficheFrais.php';
+        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $unMois );
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $unMois );
+        if(empty($lesFraisForfait)) {
+            ajouterErreur('il n y a rien a afficher');
+        include 'vues/v_erreurs.php';
+         header("Refresh: 3;URL=index.php?uc=validerFrais&action=selectionnerVisiteursMois");
+    } else {
+        include 'vues/v_valider.php';
+    }
+   break;  
+   case 'corrigerElementForfaitises' :
+        $idVisiteur = filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING);
+        $unMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+        $lesFraisForfait = filter_input(INPUT_POST, 'lesFrais', FILTER_DEFAULT,FILTER_FORCE_ARRAY);
+        //var_dump($unMois, $idVisiteur);
+        $visiteurs_a_valider = $pdo->getLesVisiteursAValider();
+        $visiteurASelectionner = $idVisiteur;
+        // var_dump($visiteurs_a_valider);
+        $mois_a_selectionner =getLesDouzeDerniersMois($mois);
+        $moisASelectionner = $unMois;
+        // var_dump($mois_a_selectionner);
+        $pdo->majFraisForfait($idVisiteur, $unMois, $lesFraisForfait);
+        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $unMois );
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $unMois );
+        include 'vues/v_valider.php';
+   break;  
+
+    case 'afficherCorrection' :
+         $id = filter_input(INPUT_POST,'id', FILTER_SANITIZE_STRING);
+         $idVisiteur = filter_input(INPUT_POST,'lstVisiteur', FILTER_SANITIZE_STRING);
+         $unMois = filter_input(INPUT_POST,'lstMois', FILTER_SANITIZE_STRING);
+         $date = filter_input(INPUT_POST,'date', FILTER_SANITIZE_STRING);
+         $libelle = filter_input(INPUT_POST,'libelle', FILTER_SANITIZE_STRING);
+         $montant = filter_input(INPUT_POST,'montant', FILTER_SANITIZE_STRING);
+        var_dump($id,$idVisiteur,$unMois,$date,$libelle,$montant);
+        $visiteurs_a_valider = $pdo->getLesVisiteursAValider();
+        $visiteurASelectionner = $idVisiteur;
+        // var_dump($visiteurs_a_valider);
+        $mois_a_selectionner =getLesDouzeDerniersMois($mois);
+        $moisASelectionner = $unMois;
+        // var_dump($mois_a_selectionner);
+       $pdo->majFraisHorsForfait($id,$idVisiteur,$unMois, $date, $libelle,$montant);
+        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $unMois );
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $unMois );
+        include 'vues/v_valider.php';
+         break;
+    case 'afficherSuppresion':
+        $id = filter_input(INPUT_POST,'id', FILTER_SANITIZE_STRING);
+         $idVisiteur = filter_input(INPUT_POST,'lstVisiteur', FILTER_SANITIZE_STRING);
+         $unMois = filter_input(INPUT_POST,'lstMois', FILTER_SANITIZE_STRING);
+         $date = filter_input(INPUT_POST,'date', FILTER_SANITIZE_STRING);
+         $libelle = filter_input(INPUT_POST,'libelle', FILTER_SANITIZE_STRING);
+         $montant = filter_input(INPUT_POST,'montant', FILTER_SANITIZE_STRING);
+        var_dump($id,$idVisiteur,$unMois,$date,$libelle,$montant);
+        $visiteurs_a_valider = $pdo->getLesVisiteursAValider();
+        $visiteurASelectionner = $idVisiteur;
+        // var_dump($visiteurs_a_valider);
+        $mois_a_selectionner =getLesDouzeDerniersMois($mois);
+        $moisASelectionner = $unMois;
+        // var_dump($mois_a_selectionner);
+       $pdo->majFraisHorsForfait($id,$idVisiteur,$unMois, $date, $libelle,$montant);
+        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $unMois );
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $unMois );
+        include 'vues/v_valider.php';
+         break;
         
-        break; 
-case 'valider' :
- $action = filter_input(INPUT_GET, 'lstMois', FILTER_SANITIZE_STRING);
-if (!$uc) {
-    $uc = 'valider';
-}
-     $visiteurs_a_valider = $pdo->getLesVisiteursAValider();
-        // var_dump($visiteurs_a_valider);
-        $mois_a_selectionner =getMoisASelectionner();
-        // var_dump($mois_a_selectionner);
-    $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $mois);
-$lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $mois);
-include 'vues/v_valider.php';
-      break; 
-   
-} 
+     
+        }
